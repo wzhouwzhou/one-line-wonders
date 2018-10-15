59d4366745f8c46a20f8faaff60ad641d79c4ad2
Romello Goodman <romellogood@gmail.com>
2018-10-14 22:41:54 -0400

    Implementing the solution found https://github.com/kannagikazuko/one-line-wonders/pull/22#issuecomment-429688085

--- javascript/page_query_params.js
+++ javascript/page_query_params.js
@@ -1,1 +1,1 @@
-const pageQueryParams = () => document.location.search.replace(/(^\?)/,'').split('&').reduce(function(o,n) { n=n.split('='); o[n[0]]=n[1]; return o},{});
+const pageQueryParams = () => document.location.search.replace(/(^\?)/, '').split('&').reduce((o, n) => (n => o[n[0]] = n[1])(n.split('=')) && o, {}) || {};
