23707d78d9656e6e118e6c37658f6c6c211f71d5
Isaac Sears <isaac.j.sears@gmail.com>
2018-10-21 14:59:00 -0400

    Make swaping more one-liner-y

--- c/bubblesort.c
+++ c/bubblesort.c
@@ -11,7 +11,7 @@ gcc bubblesort.c
 # define ITERATIONS 1000000 // more iterations for greater chance of actually sorting; fewer for more efficiency ;)
 
 void oneLinerBubbleSort(int * a, int s){
-    for(int i=0; i<ITERATIONS; i++){for(int j=1; j<s; j++){if (a[j]<a[j-1]){int t=a[j-1]; a[j-1]=a[j]; a[j]=t;}}}
+    for(int i=0; i<ITERATIONS; i++){for(int j=1; j<s; j++){if (a[j]<a[j-1]){(a[j-1]^=a[j]), (a[j]^=a[j-1]), (a[j-1]^=a[j]);}}}
 }
 
 int main(int argc, char **argv){
