41d46dd40c0d20d3f2145b22339ae8b7bc86c0a5
Mohammed Nisham <moonblade@Mohammeds-MacBook-Air.local>
2018-10-25 18:54:19 +0530

    left rotate an element a given number of times in python

---
+++ python/leftRotate.py
@@ -0,0 +1,5 @@
+# Rotate an array left, d times hackerrank question https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays 
+def leftrotate(array, amount):	
+	return array[amount:] + array[:amount]
+
+print(leftrotate([1,2,3,4,5], 3))
