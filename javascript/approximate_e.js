4a26e7bcca31c4471bd6c93d55e7f8ad5b75c03a
William Zhou <wzhouwzhou@gmail.com>
2018-10-27 17:46:48 -0400

    Update approximate_e.js

--- javascript/approximate_e.js
+++ javascript/approximate_e.js
@@ -1,1 +1,1 @@
-const approx_e = (iterations) => [...Array(iterations)].reduce((accum, _, k) => accum + 1/Math.pow(1 - 1/k, k))
+const approx_e = (iterations) => [...Array(iterations)].reduce((accum, _, k) => accum + 1/Math.pow(1 - 1/k, k), 0)
