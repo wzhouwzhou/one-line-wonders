309eff33be0fa4d4a977eb9dda06728933e3b444
William Zhou <wzhouwzhou@gmail.com>
2018-10-30 12:19:18 -0400

    Delete fibonacci_inMemory.js

--- javascript/fibonacci_inMemory.js
+++
@@ -1,3 +0,0 @@
-
-// Memory efficent fibonacci computation. No memory stack, and array.length never gets larger than 3.
-export const fib = n => n <= 0 ? 0 : n < 2 ? 1 : (a => { Array.from(Array(n), () => { a.push(a[0]+a[1]); a.shift(); }); return a.pop(); })([1,1]);
