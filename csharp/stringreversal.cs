5babce81f083f117341bb0b775b0db9bd8e99b85
William Zhou <wzhouwzhou@gmail.com>
2018-10-30 21:10:54 -0400

    Update stringreversal.cs

--- csharp/stringreversal.cs
+++ csharp/stringreversal.cs
@@ -6,7 +6,7 @@ public class Program
 	{
 		string line = Console.ReadLine();
 		
-		Func<string, string> fac = null; fac = n => (n.Length < 1) ? string.Empty : n[n.Length-1] + fac(n.Substring(0, n.Length - 1));
+		Func<string, string> fac = n => (n.Length < 1) ? string.Empty : n[n.Length-1] + fac(n.Substring(0, n.Length - 1));
 
 		Console.WriteLine(fac(line));
 	}
