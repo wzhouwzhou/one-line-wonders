9bc88c637abaec9f5ea5b472189738315014ea49
Benjamin Urquhart <24701883+BenjaminUrquhart@users.noreply.github.com>
2018-11-23 11:48:29 -0500

    CodeGolf

--- java/SumDigits.java
+++ java/SumDigits.java
@@ -1,2 +1,2 @@
 //I did them one better: put the entire class on one line (on my phone, no less)
-public class SumDigits{public static int sumDigits(int num){return (""+num).chars().reduce(0,(x,y) -> x+(y-'0'));}public static void main(String[] args){if(args.length == 0){System.out.println("Usage: SumDigits <number>"); return;}System.out.println(sumDigits(Integer.parseInt(args[0])));}}
+public class SumDigits{public static int sumDigits(int num){return (""+num).chars().reduce(0,(x,y) -> x+(y-'0'));}public static void main(String[] args){if(args.length == 0){System.out.println("Usage: SumDigits <number>");return;}System.out.println(sumDigits(Integer.parseInt(args[0])));}}
