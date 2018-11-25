214a68f48836753ac23852ffdc74eaf3549eb71f
Ashutosh Kumar Singh <33740437+ashu1999@users.noreply.github.com>
2018-11-25 11:06:37 +0530

    Update hello.c

--- c/hello/hello.c
+++ c/hello/hello.c
@@ -1,1 +1,12 @@
-main(){puts("Hello, World!");}
+#include<stdio.h>
+int main()
+{
+	int n,i;
+	printf("Enter a number:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{ 
+		printf("Hello World ");
+	}
+	return 0;
+}
