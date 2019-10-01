0e30419046c0793df77ad7fe14f5f0a94e8e4aea
Kanishk Gupta <kanishkgupta1234@gmail.com>
2019-10-01 23:30:35 +0530

    Added Rust

---
+++ dart/hello.dart
@@ -0,0 +1,9 @@
+main() {
+  String tradingSecrets = "High Frequency Trading requires extremely fast computers";
+  int latency = 10;
+  String message = "Google Dart language";
+ 
+  print('Hello world in ${message }');
+  print("This is String literal just like Java");
+  print( '${tradingSecrets}');
+}
