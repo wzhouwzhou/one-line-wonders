2438b8b9b00f1287288b61434ed9ad6b01f80f17
codetheorem <54779517+codetheorem@users.noreply.github.com>
2019-10-01 19:41:04 +0530

    Update fibonacci.c

--- c/fibonacci/fibonacci.c
+++ c/fibonacci/fibonacci.c
@@ -5,5 +5,5 @@
 int fib(int n) { return (n==0 || n==1) ? 1 : (fib(n-1) + fib(n-2)); }
 
 int main(int argc, char *argv[]) {
-  printf("Fibonacci: %d\n", fib((int) *argv[1] - '0'));
+  printf("Fibonacci Number: %d\n", fib((int) *argv[1] - '0'));
 }
