a7b48ac0cbdeaf71d10dfbf5bc4070046527ce22
subham <subham.iitrpr@gmail.com>
2019-10-04 02:16:48 +0530

    Removes Input From User Removes Input From User, instead a sample input is hard-coded

--- cplusplus/unique_array.cpp
+++ cplusplus/unique_array.cpp
@@ -31,12 +31,12 @@ using namespace std;
 
 int main(){
     int n, temp;
-    cin>>n;
+    n=8; // Size of B
+    int B[] = { 1,2,3,4,4,5,9,1};
     set<int> A;
     for(int i=0; i<n; i++){
-        cin>>temp;
-        if(binary_search(A.begin(), A.end(),temp)==0){
-            A.insert(temp);
+        if(binary_search(A.begin(), A.end(),B[i])==0){
+            A.insert(B[i]);
         }
     }
     set<int>:: iterator it = A.begin();
@@ -46,7 +46,10 @@ int main(){
     cout<<endl;
 }
 
+
 /*
+RUN THE CODE - https://ideone.com/263ng6
+
 ANALYSIS - 
     Insertion in set costs O(log(n)) in average case.
     Thus, For n insertion it will cost O(n * log(n)).
