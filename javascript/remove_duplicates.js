b90d291b6f505b205c8bdabbdbe15f12e97b56ff
Ben Carpenter <benwcarpenter0@gmail.com>
2019-10-04 18:29:14 +0930

    Created JavaScript solution to unique array issue

---
+++ javascript/remove_duplicates.js
@@ -0,0 +1,4 @@
+// Define array for duplicates to be removed
+const array = [2, 5, 2, 6, 8];
+// Create set from array (only adds non-duplicates) and print
+const unique = Array.from(new Set(array)); console.log(unique);
