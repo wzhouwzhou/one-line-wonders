7bfc29546b888c7c1eb8ad23768ec9f5c5444a6f
Amit Dutta <amitdutta2406@gmail.com>
2019-10-05 01:54:14 +0530

    Added Profile and swap oneliner

---
+++ python/Amit_swap.py
@@ -0,0 +1,8 @@
+# Python program to swap two variables in single line 
+x = int(input("Enter 1st number: "))
+y = int(input("Enter 2nd number: "))
+x, y = y, x 
+print(f"After Swapping values of 1st and 2nd are {x} {y}")
+
+
+
