960e61d5d72b207effbb2ef54357700fcdf3f2d2
Ahnaf Hadi Fathulloh <ahnafhadi@gmail.com>
2019-10-04 15:07:03 +0700

    Update some changes

--- cplusplus/remove_duplicate_elements.cpp
+++ cplusplus/remove_duplicate_elements.cpp
@@ -1,34 +1,22 @@
 #include<iostream>
- 
 using namespace std;
  
 int main()
 {
 	int i,j,k,n,a[30];
-	cout<<"How many elements? ";
-	cin>>n;
+	cout<<"How many elements? "; cin>>n;
 	cout<<"\nEnter elements of array\n";
-	
 	for(i=0;i<n;i++)
 		cin>>a[i];
-		
 	for(i=0;i<n;i++)
 		for(j=i+1;j<n;)
-		{
-			if(a[i]==a[j])
-			{
+			if(a[i]==a[j]){
 				for(k=j;k<n-1;++k)
-					a[k]=a[k+1];
-					
-				--n;
+						a[k]=a[k+1];
+						--n;
 			}
-			else
-				++j;
-		}
-	
-	cout<<"";
+			else{ ++j;}
 	for(i=0;i<n;++i)
 		cout<<a[i]<<" ";
- 
 	return 0;
 }
