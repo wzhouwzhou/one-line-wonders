fff0e63836a66c8f34e1371b37a76fa04a1e2e6b
Gowtham Pollam <gowthampollam@gmail.com>
2019-10-08 20:24:53 +0530

    Adding an amazing Python one-liner
    This code takes all the lines of a file and removes whitespaces and stores the result in a list

---
+++ python/File_filter_Input.py
@@ -0,0 +1,9 @@
+# THE DATA
+filename = 'oneliners.py'
+ 
+# THE ONE-LINER
+lines = [line.strip() for line in open(filename)]
+ 
+# THE RESULT
+print(lines)
+# ['# THE DATA', "filename = 'oneliners.py'", '', '# THE ONE-LINER', 'lines = [line.strip() for line in open(filename)]', '', '# THE RESULT', 'print(lines)', '']
