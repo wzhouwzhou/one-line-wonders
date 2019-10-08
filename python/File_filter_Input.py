43a085774e1136d70491e0511ace67eac77fc129
Gowtham Pollam <gowthampollam@gmail.com>
2019-10-08 20:32:13 +0530

    Delete File_filter_Input.py

--- python/File_filter_Input.py
+++
@@ -1,9 +0,0 @@
-# THE DATA
-filename = 'oneliners.py'
- 
-# THE ONE-LINER
-lines = [line.strip() for line in open(filename)]
- 
-# THE RESULT
-print(lines)
-# ['# THE DATA', "filename = 'oneliners.py'", '', '# THE ONE-LINER', 'lines = [line.strip() for line in open(filename)]', '', '# THE RESULT', 'print(lines)', '']
