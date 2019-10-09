bdfd0cc25dac278e7a9164f2296db3a171129808
Yash Agarwal <38498671+yashagarwal1999@users.noreply.github.com>
2019-10-09 09:40:20 +0530

    Add files via upload

---
+++ python/powerset.py
@@ -0,0 +1,7 @@
+from itertools import chain, combinations
+
+def powerset(iterable):
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+    
+print(list(powerset("abc")))
