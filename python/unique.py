f8817ccb249f269c3bb694c6815ee76c4887325f
Yash Agarwal <38498671+yashagarwal1999@users.noreply.github.com>
2019-10-19 13:10:16 +0530

    Solved   Unique Array #323
    Solved   Unique Array #323 in python

---
+++ python/unique.py
@@ -0,0 +1,10 @@
+def getunique(arr):
+    finallist=[]
+    for i in arr:
+        if(i not in finallist):
+            finallist.append(i);
+    return finallist
+        
+list1 = [10, 20, 10, 30, 40, 40] 
+print(getunique(list1))
+
