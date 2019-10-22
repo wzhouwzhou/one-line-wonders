c93f0d9d192a17ca821c32f25931443b0d7aefea
Amanda Bertsch <42593540+abertsch72@users.noreply.github.com>
2019-10-22 13:45:54 -0700

    test won't fail if seqs is changed

--- python/unique/abertsch72_unique_strings_in_nested_lists.py
+++ python/unique/abertsch72_unique_strings_in_nested_lists.py
@@ -2,5 +2,6 @@ seqs = [["a","b","c"],["a","d","e","f"],["y","a"],["a","a","a","a"], ["A"]]
 
 print(len(set([name for seq in seqs for name in seq])))
 
-
-assert (len(set([name for seq in seqs for name in seq])) == 8)
+# Testing
+testseqs = [["a","b","c"],["a","d","e","f"],["y","a"],["a","a","a","a"], ["A"]]
+assert (len(set([name for seq in testseqs for name in seq])) == 8)
