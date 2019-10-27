6bda45d8c6c95fe58a0005a1156e9fd7275d86e8
William Zhou <wzhouwzhou@gmail.com>
2019-10-27 17:47:36 -0400

    Update wzhouwzhou_unique_values.js

--- javascript/unique/wzhouwzhou_unique_values.js
+++ javascript/unique/wzhouwzhou_unique_values.js
@@ -1,3 +1,3 @@
 const values = [1, 2, 3, 4, 5, 1, 1, 1, 3];
 
-const unique = [void(0)].map((e, i, arr) => new Set(values).forEach(v => arr.push(v)) || arr)[0].slice(1)
+const unique = [void(0)].map((e, i, arr) => new Set(values).forEach(v => arr.push(v)) || arr)[0].slice(1);
