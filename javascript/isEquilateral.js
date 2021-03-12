64553362e69422f6f073cd47154511f9f224185e
GaslightGod <60765944+GaslightGod@users.noreply.github.com>
2021-03-11 21:23:29 -0600

    Update isEquilateral.js

--- javascript/isEquilateral.js
+++ javascript/isEquilateral.js
@@ -1,1 +1,1 @@
-const isEquilateral = (a, b, c, d) => a === b && a === c && a === d ? console.log('square') : console.log('not a square');
+const GaslightGod_isEquilateral = (a, b, c, d) => a === b && a === c && a === d ? console.log('square') : console.log('not a square');
